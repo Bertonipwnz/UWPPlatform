98cdac1db5fcaea5bd0e1e379d0d360c30749a71
Bertonipwnz <romashev14@mail.ru>
2022-07-17 17:40:27 +0700

    Добавлены способы компоновки

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+﻿using Windows.UI.Xaml.Controls;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
 
@@ -38,6 +25,30 @@ namespace UWPPlatform
             // Содержимое выбранного элемента
             switch (item.Name)
             {
+                case "GridWithProportionalSize":
+                    ContentFrame.Navigate(typeof(GridWithProportionalSize));
+                    break;
+                case "GridLayout":
+                    ContentFrame.Navigate(typeof(GridLayout));
+                    break;
+                case "GridWithAutoSize":
+                    ContentFrame.Navigate(typeof(GridWithAutoSize));
+                    break;
+                case "VariableSizedWrapGrid":
+                    ContentFrame.Navigate(typeof(VariableSizedWrapGrid));
+                    break;
+                case "StackPanelLayout":
+                    ContentFrame.Navigate(typeof(StackPanelLayout));
+                    break;
+                case "CanvasLayout":
+                    ContentFrame.Navigate(typeof(CanvasLayout));
+                    break;
+                case "RelativePanelLayout":
+                    ContentFrame.Navigate(typeof(RelativePanelLayout));
+                    break;
+                case "SplitViewLayout":
+                    ContentFrame.Navigate(typeof(SplitViewLayout));
+                    break;
                 case "Binding":
                     ContentFrame.Navigate(typeof(Binding));
                     break;
